bdef445e551d831baf76bf5a17c9907aba1c549d
hyunjung <hhhj.1486@gmail.com>
2024-01-12 21:17:27 +0900

    2024.01.12

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Post from './components/Post';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <main>
+        <Post author="Maximilian" body="React.js is awesome!" />
+        <Post author="Manuel" body="Check out the full course!!"/>
+      </main>
+  )
 }
 
 export default App;
